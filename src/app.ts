172146625148ac04efff7f22dc64c413c7b3f2c4
aryan <agarwalaryan139@gmail.com>
2024-01-25 02:57:23 +0530

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,32 @@
 import express from "express";
-require('dotenv').config();
+import cors from "cors";
+import compression from "compression";
+require("dotenv").config();
 
 // routes
 import auth from "./routes/auth";
 import user from "./routes/user";
 
 // middleware
-import authMiddleware from './middleware/auth';
-
+import authMiddleware from "./middleware/auth";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
-app.use('/auth', auth);
-app.use('/user', user);
+app.use(cors());
+app.use(compression());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+app.use("/auth", auth);
+app.use("/user", user);
 
 app.listen(PORT);
 
-console.log(`Server running on port ${PORT}`)
+console.log(`Server running on port ${PORT}`);
