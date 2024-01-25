255ba239cb78dfb6ab1cd82d28532fecee170933
aryan <agarwalaryan139@gmail.com>
2024-01-26 04:06:12 +0530

    fix: login encryption issue

--- src/utils/farcaster/generateKeypair.ts
+++ src/utils/farcaster/generateKeypair.ts
@@ -1,6 +1,6 @@
 import Web3 from "web3";
 import prisma from "../clients/prisma";
-import { createCipheriv } from "crypto";
+import { createCipheriv, createDecipheriv } from "crypto";
 
 export const getOrCreatePublicKey = async (userId: string) => {
   let auth = await prisma.auth.findUnique({
@@ -15,22 +15,39 @@ export const getOrCreatePublicKey = async (userId: string) => {
   if (!auth) {
     const web3 = new Web3(process.env.OP_RPC_URL);
 
+    console.log("Creating new keypair...");
+
     const account = web3.eth.accounts.create();
+
+    console.log("Keypair created!");
 
     const encryption_secret = process.env.ENCRYPTION_KEY as string;
     const iv = process.env.IV as string;
 
-    const cipher = createCipheriv("aes-256-cbc", encryption_secret, iv);
+    const cipher = createCipheriv(
+      "aes-256-cbc",
+      Buffer.from(encryption_secret, "hex"),
+      Buffer.from(iv, "hex")
+    );
+
+    console.log("Encrypting keypair...");
 
     const encrypted_secret =
       cipher.update(account.privateKey, "utf8", "hex") + cipher.final("hex");
 
-    // function decrypt(encryptedText: string) {
-    //     const decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(encryptionKey), Buffer.from(iv));
-    //     let decrypted = decipher.update(encryptedText, 'hex', 'utf-8');
-    //     decrypted += decipher.final('utf-8');
-    //     return decrypted;
-    //   }
+    console.log("Keypair encrypted!");
+
+    function decrypt(encryptedText: string) {
+      const decipher = createDecipheriv(
+        "aes-256-cbc",
+        Buffer.from(encryption_secret, "hex"),
+        Buffer.from(iv, "hex")
+      );
+      let decrypted = decipher.update(encryptedText, "hex", "utf-8");
+      decrypted += decipher.final("utf-8");
+      console.log(decrypted);
+      return decrypted;
+    }
 
     await prisma.auth.create({
       data: {
