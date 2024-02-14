dfc82a75b601eb36995bc10e46a02bdc26f11e52
aryan <agarwalaryan139@gmail.com>
2024-02-14 23:28:29 +0530

    fix: add cursor

--- src/utils/user/getFeed.ts
+++ src/utils/user/getFeed.ts
@@ -2,10 +2,12 @@ import { fc } from "../clients/fc";
 import axios from "axios";
 import { NeynarCast } from "../../types";
 
-const getFeed = async (limit?: number) => {
+const getFeed = async (limit?: number, cursor?: string) => {
   if (!limit) limit = 25;
   if (limit > 100) throw new Error("Limit must be less than 100");
-  const feed_api = `https://api.neynar.com/v2/farcaster/feed/frames?limit=${limit}`;
+  const feed_api =
+    `https://api.neynar.com/v2/farcaster/feed/frames?limit=${limit}` +
+    (cursor ? `&cursor=${cursor}` : "");
   const feed = await axios.get(feed_api, {
     headers: {
       api_key: process.env.NEYNAR_API_KEY as string,
@@ -13,6 +15,8 @@ const getFeed = async (limit?: number) => {
   });
 
   const casts = feed.data.casts as NeynarCast[];
+
+  cursor = feed.data.next?.cursor as string;
 
   let feed_text = [] as any[];
   for (let i = 0; i < casts.length; i++) {
@@ -27,13 +31,16 @@ const getFeed = async (limit?: number) => {
       embeds: casts[i].embeds,
       hash: casts[i].hash,
       timestamp: casts[i].timestamp,
-      reaction : {
-        LIKE : casts[i].reactions?.likes.length,
-        RECAST : casts[i].reactions?.recasts.length,
-      }
+      reaction: {
+        LIKE: casts[i].reactions?.likes.length,
+        RECAST: casts[i].reactions?.recasts.length,
+      },
     });
   }
-  return feed_text;
+  return {
+    feed: feed_text,
+    cursor: cursor,
+  };
 };
 
 export default getFeed;
